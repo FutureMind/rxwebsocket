bebbf7796da7b235a4c89f32939e9f5e8ffc3554
Michal Klimczak <m.klimczak@futuremind.com>
2020-05-18 16:19:30 +0200

    Properly spy RealWebSocket with Mockk

--- rxwebsocket/src/test/java/com/futuremind/rxwebsocket/RxWebSocketTest.kt
+++ rxwebsocket/src/test/java/com/futuremind/rxwebsocket/RxWebSocketTest.kt
@@ -1,33 +1,55 @@
 package com.futuremind.rxwebsocket
 
-import io.mockk.every
-import io.mockk.mockk
+import io.mockk.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+import okhttp3.internal.concurrent.TaskRunner
+import okhttp3.internal.ws.RealWebSocket
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.util.*
 
 
 class RxWebSocketTest {
 
-    private val realSocket : WebSocket = mockk()
-    private val okHttpClient : OkHttpClient = mockk()
-    private val request = Request.Builder().url("https://what.ever").build()
+    lateinit var realSocket: WebSocket
+    private var okHttpClient: OkHttpClient = mockk()
+    private var request = Request.Builder().url("https://what.ever").build()
 
-    private lateinit var rxSocket : RxWebSocket
+    private lateinit var rxSocket: RxWebSocket
 
     @BeforeEach
-    fun setUp(){
+    fun setUp() {
         rxSocket = RxWebSocket(okHttpClient, request)
-        every { okHttpClient.newWebSocket(request, any()) } returns realSocket
+
+        val initRequest = slot<Request>()
+        val initListener = slot<WebSocketListener>()
+        every {
+            okHttpClient.newWebSocket(
+                capture(initRequest),
+                capture(initListener)
+            )
+        } answers {
+            realSocket = spyk(
+                RealWebSocket(
+                    TaskRunner.INSTANCE,
+                    initRequest.captured,
+                    initListener.captured,
+                    Random(),
+                    0
+                )
+            )
+            realSocket
+        }
     }
 
     @Test
     fun `given subscribed to rxws, immediately returns Connecting Status`() {
         rxSocket.connect()
             .test()
-            .assertValue{ it is SocketState.Connecting }
+            .assertValue { it is SocketState.Connecting }
     }
 
     @Test
@@ -35,6 +57,20 @@ class RxWebSocketTest {
         rxSocket.connect()
             .test()
             .assertNotComplete()
+    }
+
+    @Test
+    fun `given disconnect called, returns Disconnected and then Completes`() {
+
+        val testSubscriber = rxSocket.connect().test()
+
+        rxSocket.disconnect(1000, "")
+
+        verify { realSocket.close(1000, "") }
+
+//        testSubscriber
+//            .assertValueAt(1, SocketState.Disconnected)
+//            .assertComplete()
     }
 
 }
